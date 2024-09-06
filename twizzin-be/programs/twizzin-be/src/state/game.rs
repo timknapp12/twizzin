81ac4e664cf1cd7363183d5d77b2b02ba99cf7d8
Tim Knapp <timknapp12@gmail.com>
2024-09-06 17:55:32 -0600

    add struct for treasurt pub key

--- twizzin-be/programs/twizzin-be/src/state/game.rs
+++ twizzin-be/programs/twizzin-be/src/state/game.rs
@@ -1,6 +1,15 @@
 use anchor_lang::prelude::*;
 
 #[account]
+pub struct ProgramConfig {
+    pub treasury_pubkey: Pubkey,
+}
+
+impl Space for ProgramConfig {
+    const INIT_SPACE: usize = 8 + 32;
+}
+
+#[account]
 // GAME ACCOUNT will serve as the escrow
 pub struct Game {
     pub admin: Pubkey,
