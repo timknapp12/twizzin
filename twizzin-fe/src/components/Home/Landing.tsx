1afbea2373eafb299f77401a06679810bb33009d
Tim Knapp <timknapp12@gmail.com>
2024-09-12 13:25:11 -0600

    update landing page

--- twizzin-fe/src/components/Home/Landing.tsx
+++ twizzin-fe/src/components/Home/Landing.tsx
@@ -1,4 +1,4 @@
-import { FaXTwitter } from 'react-icons/fa6';
+import { FaXTwitter, FaTelegram } from 'react-icons/fa6';
 import { Column, Row } from '../containers';
 import { H3, H3Brand } from '../texts';
 
@@ -9,18 +9,27 @@ export const Landing = () => {
         <Column className='items-center'>
           <H3Brand className='mt-2 mb-2'>TWIZZIN</H3Brand>
           <H3>
-            is currently in development. Follow us on X to stay updated on our
-            progress.
+            is currently in development. Follow us on X and Telegram to stay
+            updated on our progress.
           </H3>
         </Column>
       </Row>
-      <a
-        href='https://x.com/twizzinapp'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <FaXTwitter size={48} style={{ cursor: 'pointer' }} />
-      </a>
+      <Row className='gap-8'>
+        <a
+          href='https://x.com/twizzinapp'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaXTwitter size={48} style={{ cursor: 'pointer' }} />
+        </a>
+        <a
+          href='https://t.me/+Xp_4og3g-dIwNWZh'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaTelegram size={48} style={{ cursor: 'pointer' }} />
+        </a>
+      </Row>
     </Column>
   );
 };
