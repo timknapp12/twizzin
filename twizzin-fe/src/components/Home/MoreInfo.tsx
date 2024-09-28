1da07e3f8091f44f22b58f4e6539167a89e8965d
Tim Knapp <timknapp12@gmail.com>
2024-09-28 10:31:49 -0600

    new style for more info section

--- twizzin-fe/src/components/Home/MoreInfo.tsx
+++ twizzin-fe/src/components/Home/MoreInfo.tsx
@@ -1,67 +1,65 @@
 import React from 'react';
-import { Button, Row, Column } from '@/components';
-import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
+import { Column, H5, GradientContainer, Row, H5Brand } from '@/components';
 import { useTranslation } from 'react-i18next';
 
-const MoreInfo = ({
-  isOpen,
-  toggleMoreInfo,
-}: {
-  isOpen: boolean;
-  toggleMoreInfo: () => void;
-}) => {
+const MoreInfo = () => {
   const { t } = useTranslation();
   return (
-    <Button
-      onClick={toggleMoreInfo}
-      className='w-full transition-all duration-700 ease-in-out p-4'
-    >
-      <Column className='w-full'>
-        <Row className='w-full relative'>
-          <span className='flex-grow'>
-            {isOpen ? t('Gimme less info') : t('Gimme more info')}
-          </span>
-          {isOpen ? (
-            <FaChevronUp size={18} className='hidden sm:block' />
-          ) : (
-            <FaChevronDown size={18} className='hidden sm:block' />
-          )}
+    <GradientContainer>
+      <Column align='start' className='w-full'>
+        <Row>
+          <H5Brand className='mr-2'>TWIZZIN</H5Brand>
+          <p>{` [twi' z' in] ${t('verb, informal')}`}</p>
         </Row>
-        <div
-          className='overflow-hidden transition-[max-height] duration-700 ease-in-out'
-          style={{ maxHeight: isOpen ? '1000px' : '0px' }}
-        >
-          <p className='text-sm text-left'>
-            {`twizzin [twi' z' in] ${t('verb, informal')}`}
-            <br />
-            {`1. ${t(
-              'To be in a state of heightened mental acuity and knowledge recall, particularly while answering trivia questions.'
-            )}`}
-            <br />
-            {`2. ${t(
-              'To perform exceptionally well in a quiz or trivia competition, as if possessing expert-level knowledge or magical, wizzard-like abilities.'
-            )}`}
-            <br />
-            <br />
-            {t(
-              `Twizzin is an interactive Web3 trivia game built on the Solana blockchain, where players compete against each other in real time.`
-            )}
-            {t(
-              `To join a game, players must pay an entry fee in SOL tokens, which contributes to the prize pool.`
-            )}
-            {t(
-              `Each round, everyone answers the same set of multiple-choice questions, earning points based on speed and accuracy.`
-            )}
-            {t(
-              `The top performers on the leaderboard are rewarded with a share of the prize pool.`
-            )}
-            {t(
-              `Each player is also rewarded with experience points for participating in the game.`
-            )}
-          </p>
-        </div>
+        <p>
+          {`1. ${t(
+            'To be in a state of heightened mental acuity and knowledge recall, particularly while answering trivia questions.'
+          )}`}
+          <br />
+          {`2. ${t(
+            'To perform exceptionally well in a quiz or trivia competition, as if possessing expert-level knowledge or magical, wizzard-like abilities.'
+          )}`}
+        </p>
+        <br />
+        <br />
+        <H5>Proof of Learn</H5>
+        <p>
+          {`${t(
+            `Twizzin is an interactive Web3 game that embraces the “Learn to Earn” model, making learning in Web3 fun and rewarding.`
+          )} `}
+          {`${t(
+            `Twizzin helps projects and DAOs incentivize their users and voters to truly understand their protocols and proposals.`
+          )} `}
+        </p>
+        <br />
+        <H5>{t(`But that's not all`)}</H5>
+        <p>
+          {`${t(
+            `Twizzin also hosts live trivia games for anyone and everyone, allowing players to put their money where their mouth is by competing in real-time with others around the world.`
+          )} `}
+          {t(
+            `These global games will attract a new user base to Web3 and Solana.`
+          )}
+        </p>
+        <br />
+        <H5>{t(`But why?`)}</H5>
+        <p>
+          {`${t(
+            `We believe in creating a better world and having fun along the way. Crypto adoption has surged, but there is still much work to be done.`
+          )} `}
+          {`${t(
+            `By offering a fun, incentivized trivia game, we aim to onboard more users and give them a positive first experience with crypto, especially on Solana—the people's chain.`
+          )} `}
+          {t(
+            `By leveraging Twizzin, we can help new users navigate the risks of the crypto space—like scams, rug pulls, and drained wallets—while introducing them to the impressive products that Solana’s elite builders are shipping.`
+          )}
+          <br />
+          <br />
+          {`${t(`Crypto needs a better way to teach users.`)} `}
+          {t(`Crypto needs Twizzin.`)}
+        </p>
       </Column>
-    </Button>
+    </GradientContainer>
   );
 };
 
