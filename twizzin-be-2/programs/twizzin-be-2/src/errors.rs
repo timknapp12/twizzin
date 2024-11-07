8a723a13c05fa7be8cf1faee6aed037e930f4982
Tim Knapp <timknapp12@gmail.com>
2024-11-07 15:49:55 -0700

    add more error messages

--- twizzin-be-2/programs/twizzin-be-2/src/errors.rs
+++ twizzin-be-2/programs/twizzin-be-2/src/errors.rs
@@ -12,4 +12,16 @@ pub enum ErrorCode {
     TreasuryAddressBlank,
     #[msg("Authority address is blank")]
     AuthorityAddressBlank,
+    #[msg("Name has to be between 1 and 32 characters")]
+    NameTooLong,
+    #[msg("Game code has to be between 1 and 6 characters")]
+    GameCodeTooLong,
+    #[msg("Max winners has to be between 1 and 10")]
+    MaxWinnersTooHigh,
+    #[msg("Start time is greater than end time")]
+    InvalidTimeRange,
+    #[msg("Token mint is required")]
+    TokenMintRequired,
+    #[msg("Vault is required")]
+    VaultRequired,
 }
