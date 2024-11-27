5a2e7267851944ac5c50b123c3156682813c10a1
Tim Knapp <timknapp12@gmail.com>
2024-11-27 08:33:28 -0700

    added output standalone in next config

--- twizzin-fe/next.config.mjs
+++ twizzin-fe/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
