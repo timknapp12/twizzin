1a6198855fb64938cf40e3c21c27729583719ece
Tim Knapp <timknapp12@gmail.com>
2024-11-27 08:59:07 -0700

    added server actions to next config

--- twizzin-fe/next.config.mjs
+++ twizzin-fe/next.config.mjs
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    serverActions: true,
+  },
   reactStrictMode: true,
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
-  transpilePackages: [
-    '@solana/wallet-adapter-base',
-    '@solana/wallet-adapter-react',
-    '@solana/wallet-adapter-react-ui',
-    '@solana/wallet-adapter-wallets',
-    '@solana/wallet-adapter-phantom',
-  ],
 };
 
 export default nextConfig;
