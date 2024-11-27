55240332e1bee5e005ae5060cd1638fd4b20a022
Tim Knapp <timknapp12@gmail.com>
2024-11-27 01:09:11 -0700

    redo routes

--- twizzin-fe/src/app/[lang]/layout.tsx
+++ twizzin-fe/src/app/[lang]/layout.tsx
@@ -1,14 +1,8 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../../i18n';
 import { ErrorBoundary } from '@/components';
-import dynamic from 'next/dynamic';
-
-const WalletProviders = dynamic(() => import('@/components/WalletProviders'), {
-  ssr: false,
-  loading: () => <div>Loading wallet...</div>,
-});
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -19,23 +13,17 @@ type LayoutProps = {
 
 export default function Layout({ children, params }: LayoutProps) {
   const resolvedParams = React.use(params);
-  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    setIsMounted(true);
     if (resolvedParams.lang) {
       i18n.changeLanguage(resolvedParams.lang);
     }
   }, [resolvedParams.lang]);
 
-  if (!isMounted) {
-    return null;
-  }
-
   return (
     <ErrorBoundary>
       <I18nextProvider i18n={i18n} defaultNS='common'>
-        <WalletProviders>{children}</WalletProviders>
+        {children}
       </I18nextProvider>
     </ErrorBoundary>
   );
