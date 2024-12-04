989a869289d5d9539a063505e883d42d63ab3f98
Tim Knapp <timknapp12@gmail.com>
2024-12-03 17:41:43 -0700

    rewards view and rewards banner

---
+++ twizzin-fe/src/components/Home/RewardsView/RewardsBanner.tsx
@@ -0,0 +1,30 @@
+import { Column, PrimaryText } from '@/components';
+import { LuPartyPopper } from 'react-icons/lu';
+import { useAppContext } from '@/contexts/AppContext';
+
+const RewardsBanner = ({ rewards = 2 }: { rewards: number }) => {
+  const { t } = useAppContext();
+  return (
+    <div className='flex h-[70px] w-full p-[14px] justify-between items-center rounded-[14px] bg-[#F9F9FA] hover:bg-[#F5F5F7]'>
+      <Column align='start'>
+        <LuPartyPopper size={20} color='var(--color-primary)' />
+        <PrimaryText style={{ fontSize: 16 }}>{t('Rewards')}</PrimaryText>
+      </Column>
+      <div className='flex justify-center items-center bg-[#6E3FFC0F] w-[50px] h-[50px] rounded-full'>
+        <Column>
+          <PrimaryText
+            style={{
+              fontSize: 22,
+              lineHeight: 1,
+              color: 'var(--color-primary)',
+            }}
+          >
+            {rewards}
+          </PrimaryText>
+        </Column>
+      </div>
+    </div>
+  );
+};
+
+export default RewardsBanner;
---
+++ twizzin-fe/src/components/Home/RewardsView/index.tsx
@@ -0,0 +1,31 @@
+import { FaArrowLeft } from 'react-icons/fa6';
+import { IconButton, Column, Row, PrimaryText } from '@/components';
+import RewardsBanner from './RewardsBanner';
+import { useAppContext } from '@/contexts/AppContext';
+
+interface RewardsViewProps {
+  // eslint-disable-next-line no-unused-vars
+  onSetView: (view: string) => void;
+}
+
+const RewardsView = ({ onSetView }: RewardsViewProps) => {
+  const { t } = useAppContext();
+  return (
+    <Column className='gap-4 w-full' align='start'>
+      <Row onClick={() => onSetView('home')} className='cursor-pointer'>
+        <IconButton
+          title='Back'
+          Icon={FaArrowLeft}
+          onClick={() => onSetView('home')}
+          className='text-gray active:text-black/10'
+        />
+        <PrimaryText style={{ fontSize: 16 }}>{t('Back')}</PrimaryText>
+      </Row>
+      <Column className='w-full'>
+        <RewardsBanner rewards={2} />
+      </Column>
+    </Column>
+  );
+};
+
+export default RewardsView;
