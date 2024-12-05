a358b0cb5dcc8cef6494024980ff94b95759437e
Tim Knapp <timknapp12@gmail.com>
2024-12-04 22:35:59 -0700

    give disabled prop to claim button

--- twizzin-fe/src/components/buttons/ClaimButton.tsx
+++ twizzin-fe/src/components/buttons/ClaimButton.tsx
@@ -12,7 +12,9 @@ export const ClaimButton = ({
     <button
       disabled={disabled}
       className={`inline-flex px-[14px] py-[10px] justify-center items-center gap-[10px] flex-shrink-0 rounded-[30px] ${
-        disabled ? 'bg-disabled text-secondaryText' : 'bg-primary  text-white'
+        disabled
+          ? 'bg-disabled text-secondaryText'
+          : 'bg-primary  text-white hover:bg-darkPurple'
       } shadow-[0px_1px_2px_0px_rgba(9,8,23,0.05)]`}
       onClick={onClick}
     >
