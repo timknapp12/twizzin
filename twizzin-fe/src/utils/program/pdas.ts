2aeaa58c1e4617dc147101e7ae656b5b8175ac43
Tim Knapp <timknapp12@gmail.com>
2025-02-06 10:38:23 -0700

    add util to generate pdas

---
+++ twizzin-fe/src/utils/program/pdas.ts
@@ -0,0 +1,34 @@
+import { PublicKey } from '@solana/web3.js';
+import { Program } from '@coral-xyz/anchor';
+import { TwizzinIdl } from '../../types/idl';
+
+export const deriveGamePDAs = (
+  program: Program<TwizzinIdl>,
+  adminPubkey: PublicKey,
+  gameCode: string
+) => {
+  const [gamePda] = PublicKey.findProgramAddressSync(
+    [Buffer.from('game'), adminPubkey.toBuffer(), Buffer.from(gameCode)],
+    program.programId
+  );
+
+  const [vaultPda] = PublicKey.findProgramAddressSync(
+    [Buffer.from('vault'), adminPubkey.toBuffer(), Buffer.from(gameCode)],
+    program.programId
+  );
+
+  return { gamePda, vaultPda };
+};
+
+export const derivePlayerPDA = (
+  program: Program<TwizzinIdl>,
+  gamePda: PublicKey,
+  playerPubkey: PublicKey
+) => {
+  const [playerPda] = PublicKey.findProgramAddressSync(
+    [Buffer.from('player'), gamePda.toBuffer(), playerPubkey.toBuffer()],
+    program.programId
+  );
+
+  return playerPda;
+};
