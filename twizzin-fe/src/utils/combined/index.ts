6c3c85911dd795e2085ac412c8480d8d52ce9c07
Tim Knapp <timknapp12@gmail.com>
2025-03-25 15:46:04 -0600

    add claim functionailty to app

--- twizzin-fe/src/utils/combined/index.ts
+++ twizzin-fe/src/utils/combined/index.ts
@@ -1,3 +1,4 @@
 export * from './createGameCombined';
 export * from './joinGameCombined';
 export * from './submitAnswersCombined';
+export * from './claimCombined';
--- twizzin-fe/src/utils/supabase/index.ts
+++ twizzin-fe/src/utils/supabase/index.ts
@@ -2,7 +2,7 @@ export * from './getGameFromDb';
 export * from './imageProcessing';
 export * from './createGame';
 export * from './supabaseClient';
-export * from './rewards';
 export * from './xp';
 export * from './getUserProfile';
 export * from './getGameResults';
+export * from './addClaimedToDb';
