466a75765233a4bf64c90b672b6ed622d23a437f
Tim Knapp <timknapp12@gmail.com>
2025-04-18 15:44:45 -0600

    update landing page

--- twizzin-fe/src/components/Home/Landing.tsx
+++ twizzin-fe/src/components/Home/Landing.tsx
@@ -32,6 +32,17 @@ export const Landing = () => {
           <FaTelegram size={48} style={{ cursor: 'pointer' }} />
         </a>
       </Row>
+      <H3 className='text-center'>
+        You can also checkout our beta version on devnet here:{' '}
+        <a
+          href='https://twizzin.vercel.app/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='underline'
+        >
+          devnet.twizzin.xyz
+        </a>
+      </H3>
     </Column>
   );
 };
