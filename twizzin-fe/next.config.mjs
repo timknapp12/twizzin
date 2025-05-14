34cd75dcfd9a22ceadff07998f3a6b63b1c67c22
Tim Knapp <timknapp12@gmail.com>
2025-05-13 20:47:19 -0600

    add localhost to next config

--- twizzin-fe/next.config.mjs
+++ twizzin-fe/next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
   output: 'standalone',
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+      bodySizeLimit: '2mb',
+    },
   },
   reactStrictMode: true,
   webpack: (config) => {
